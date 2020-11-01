ee80fa672052af42f4cc7294669788409f5b5dec
wai2016 <jackietong2014@gmail.com>
2020-11-01 19:27:24 +0800

    create demo app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "user",
+      password: "password",
+      database: "product",
+      entities: ["**/*.entity{.ts,.js}"],
+      synchronize: true
+    }),
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
